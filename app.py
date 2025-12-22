83a9327d4e6fc510ba05b2e38953a2b96a9c9f0a
5takarcoin <5takarcoin@gmail.com>
2025-12-22 13:22:20 +0600

    Login/Signup basic

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request, redirect
 from db import get_db_connection
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
@@ -33,12 +34,65 @@ def get_users():
 
     return "Connecting DB"
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        user = request.form.get('username')
+        pw = request.form.get('password')
+
+        connection = get_db_connection()
+        cursor = connection.cursor()        
+
+        sql = "SELECT first_name, password FROM players WHERE username = %s"
+
+        cursor.execute(sql, (user))
+        result = cursor.fetchone()
+        if result:
+            stored_hash = result['password']
+            first_name = result['first_name']
+
+            if check_password_hash(stored_hash, pw):
+                print(f"Success! Welcome {first_name}")
+                return f"Welcome {first_name}!" 
+            else:
+                print("Failed: Wrong password.")
+                return render_template("login.html", error="Invalid password")
+        else:
+            print("Failed: Username not found.")
+            return render_template("login.html", error="User does not exist")
+
+        cursor.close()
+        connection.close()
+
     return render_template("login.html")
 
-@app.route("/signup")
+
+
+@app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        fname = request.form.get('first_name')
+        lname = request.form.get('last_name')
+        uname = request.form.get('username')
+        email = request.form.get('email')
+        dob   = request.form.get('dob')
+        unhashed_password = request.form.get('password')
+        password = generate_password_hash(unhashed_password) 
+
+        connection = get_db_connection()
+        cursor = connection.cursor()
+        
+        sql = """
+        INSERT INTO players (first_name, last_name, username, email, password, date_of_birth)
+        VALUES (%s, %s, %s, %s, %s, %s)
+        """
+        print("Dekho", fname, uname, dob, password)
+        cursor.execute(sql, (fname, lname, uname, email, password, dob))
+        connection.commit()        
+        cursor.close()
+        connection.close()
+
+        return redirect('/login')
     return render_template("signup.html")
 
 @app.route("/friends", methods=['GET', 'POST'])
--- db.py
+++ db.py
@@ -2,10 +2,10 @@ import pymysql
 
 def get_db_connection():
     return pymysql.connect(
-        host="localhost",
+        host="127.0.0.1",
         user="root",
         password="",
         database="cse370",
         port=3306,
-        # cursorclass=pymysql.cursors.DictCursor
+        cursorclass=pymysql.cursors.DictCursor
     )
