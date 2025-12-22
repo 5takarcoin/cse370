00b49a83a481a9469eb705bc466c995aedf68aa0
Shaiara Noshin <shaiara.noshin@g.bracu.ac.bd>
2025-12-22 09:14:41 +0600

    Create query for leaderboard

--- queries.sql
+++ queries.sql
@@ -4,4 +4,9 @@
 SELECT ga.game_name, gr.game_genre
 FROM games ga
 JOIN game_genres gr ON gr.game_id = ga.game_id;
--- more comments
+-- highest scorer
+SELECT p.username, gs.score, gs.session_start_time
+FROM game_session gs
+JOIN player p ON gs.player_id = p.player_id
+WHERE gs.game_id = ?
+ORDER BY gs.score DESC;
